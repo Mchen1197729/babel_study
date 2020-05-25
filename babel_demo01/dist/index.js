276f90a6083b765511be3b82ba60f9a1810525ed
chenMM <1197729488@qq.com>
2020-05-25 21:38:49 +0800

    first commit

---
+++ babel_demo01/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _add = require("./add");
+
+console.log([1, 2, 3, 4, 5].map(x => x * x));
+console.log((0, _add.add)(10, 20));
---
+++ babel_demo01/src/index.js
@@ -0,0 +1,5 @@
+import {add} from './add'
+
+console.log([1, 2, 3, 4, 5].map(x => x * x))
+
+console.log(add(10, 20))
